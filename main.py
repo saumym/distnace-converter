e2f3d9b69a34b360c0d49238c90304f83b8f66ef
Mahesh Jadhav <omkar28399@gmail.com>
2021-05-23 17:22:15 +0530

    Labels and Display fields added

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from tkinter import ttk
 root=tk.Tk()
 root.title("Distance converter")
 
+meter_label=ttk.Label(main,text='Meter :',font=('Segoe UI',15))
+meter_input=ttk.Entry(main,width=10,textvariable=meters_value,font=('Segoe UI',15))
+meter_input.focus()
+
+feet_label=ttk.Label(main,text='Feet :',font=('Segoe UI',15))
+feet_display=ttk.Label(main,textvariable=feets_value,font=('Segoe UI',15))
+
 
 root.mainloop()
