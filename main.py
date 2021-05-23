ae8eadfd3770ad926db2cfe86e59a2bede985d35
Mahesh Jadhav <omkar28399@gmail.com>
2021-05-23 17:26:31 +0530

    grid layout added for labels,input and display field

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ meter_input.focus()
 feet_label=ttk.Label(main,text='Feet :',font=('Segoe UI',15))
 feet_display=ttk.Label(main,textvariable=feets_value,font=('Segoe UI',15))
 
+meter_label.grid(row=0,column=0,sticky='w')
+meter_input.grid(row=0,column=1,sticky='we')
+feet_label.grid(row=1,column=0,sticky='w')
+feet_display.grid(row=1,column=1,sticky="we")
 
 root.mainloop()
